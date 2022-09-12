efa41c2fe38ea641d9ded5fc28f81b8b8fb83b80
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-12 23:41:05 +0200

    footer, knapper

--- drikke-kalkulator/src/App.js
+++ drikke-kalkulator/src/App.js
@@ -3,6 +3,7 @@ import "./style.css"
 import Navbar from "./components/Navbar.js"
 import Header from "./components/Header.js"
 import Info from "./components/Info.js"
+import Footer from "./components/Footer.js"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Header />
       <Info />
+      <Footer />
     </>
   );
 }
---
+++ drikke-kalkulator/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-dark text-center mt-5 shadow'>
+        <div className='container text-white pt-4 pb-2'>
+            <p>&copy; DrikkeKalkulatoren | Laget av Ole, Alexander og Frederik</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
