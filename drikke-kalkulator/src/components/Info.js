9dbcc932c02e23f0d3389161383c4fdbf2f92b53
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-12 11:30:20 +0200

    fiksa

--- drikke-kalkulator/src/components/Info.js
+++ drikke-kalkulator/src/components/Info.js
@@ -5,26 +5,27 @@ function Info() {
     <div className='text-center'>
         <div className='container text-white bg-dark my-5 py-4 shadow'>
             <h1 className='display-6'>Vårt Konsept</h1>
-            <h4>Er du lei av å søke/kalkulere prisen på forskjellige mengder drikkevarer? <br></br> Hos oss 
+            <p>
+                Er du lei av å søke/kalkulere prisen på forskjellige mengder drikkevarer? <br></br> Hos oss 
                 kan du enkelt beregne hvor mye drikke du burde kjøpe og hva det vil koste hos de forskjellig levrandørene.
-            </h4>
+            </p>
         </div>
         <div className='container'>
             <div className='row g-5'>
                 <div className='col-sm'>
                     <h1 className='display-6'>Hei</h1>
                     <hr />
-                    <p>Heihei</p>
+                    <p>Ikke skriv noe her ennå</p>
                 </div>
                 <div className='col-sm'>
                     <h1 className='display-6'>Hei</h1>
                     <hr />
-                    <p>Heihei</p>
+                    <p>Ikke skriv noe her ennå</p>
                 </div>
                 <div className='col-sm'>
                     <h1 className='display-6'>Hei</h1>
                     <hr />
-                    <p>Heihei</p>
+                    <p>Ikke skriv noe her ennå</p>
                 </div>
             </div>
         </div>
