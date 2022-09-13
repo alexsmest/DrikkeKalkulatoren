46dcea356c3ea100e745e7a7de0d17cdc9b0c583
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 10:22:40 +0200

    adda support for side skifting

--- drikke-kalkulator/src/App.js
+++ drikke-kalkulator/src/App.js
@@ -1,18 +1,25 @@
 import "./bootstrap/css/bootstrap.css"
 import "./style.css"
+
 import Navbar from "./components/Navbar.js"
 import Header from "./components/Header.js"
 import Info from "./components/Info.js"
 import Footer from "./components/Footer.js"
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Header />
-      <Info />
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Info />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
