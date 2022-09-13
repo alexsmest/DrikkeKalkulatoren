306b9fcede9754087775e7c6a8ee5bb8f91df1a3
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 18:26:43 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='bg-dark text-center mt-5 shadow'>
         <div className='container text-white pt-4 pb-2'>
-            <p>&copy; DrikkeKalkulatoren | Laget med kjærlighet av Ole, Alexander, Frederik og Erik</p>
+            <p>&copy; DrikkeKalkulatoren | Laget av Ole, Alexander, Frederik og Erik</p>
         </div>
     </div>
   )
