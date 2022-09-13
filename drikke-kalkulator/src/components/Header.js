b1d098c31912c9fe84ecb578c105eaf1ced31488
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 12:22:55 +0200

    Update Header.js

--- drikke-kalkulator/src/components/Header.js
+++ drikke-kalkulator/src/components/Header.js
@@ -12,27 +12,27 @@ function Header() {
         <div className='col-lg border p-3'>
           <img src={Icon} alt="" className='img-responsive' width={200} />
           <h1 className='display-6'>Brennevin</h1>
-          <Link to="/Brennevin" className='btn btn-primary'>Se mer</Link>
+          <Link to="/Brennevin" className='btn btn-primary'>Se utvalg</Link>
         </div>
         <div className='col-lg border p-3'>
         <img src={Icon} alt="" className='img-responsive' width={200} />
           <h1 className='display-6'>Sterkvin</h1>
-          <Link to="/Sterkvin" className='btn btn-primary'>Se mer</Link>
+          <Link to="/Sterkvin" className='btn btn-primary'>Se utvalg</Link>
         </div>
         <div className='col-lg border p-3'>
           <img src={Icon} alt="" className='img-responsive' width={200} />
           <h1 className='display-6'>Vin</h1>
-          <Link to="/Vin" className='btn btn-primary'>Se mer</Link>
+          <Link to="/Vin" className='btn btn-primary'>Se utvalg</Link>
         </div>
         <div className='col-lg border p-3'>
           <img src={Icon} alt="" className='img-responsive' width={200} />
           <h1 className='display-6'>Øl</h1>
-          <Link to="/Øl" className='btn btn-primary'>Se mer</Link>
+          <Link to="/Øl" className='btn btn-primary'>Se utvalg</Link>
         </div>
         <div className='col-lg border p-3'>
           <img src={Icon} alt="" className='img-responsive' width={200} />
           <h1 className='display-6'>Cider</h1>
-          <Link to="/Cider" className='btn btn-primary'>Se mer</Link>
+          <Link to="/Cider" className='btn btn-primary'>Se utvalg</Link>
         </div>
       </div>
     </div>
