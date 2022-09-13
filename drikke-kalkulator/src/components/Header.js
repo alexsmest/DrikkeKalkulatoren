fdb65a7aaa543306ff2ba44cc644515dbd6f3a10
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 11:29:53 +0200

    adda knapper

--- drikke-kalkulator/src/components/Header.js
+++ drikke-kalkulator/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { Link } from "react-router-dom"
+import Icon from "../images/flaske.png"
 
 function Header() {
   return (
@@ -8,8 +8,33 @@ function Header() {
       <h1 className='display-4 mt-5'>DrikkeKalkulatoren</h1>
       <hr />
       <p>Drikk deg dritings</p>
-      <Link to="/Promillekalkulator" className='btn btn-primary me-2'>Promillekalkulator</Link>
-      <Link to="/Priskalkulator" className='btn btn-primary'>Priskalkulator</Link>
+      <div className='row text-center mt-5'>
+        <div className='col-lg border p-3'>
+          <img src={Icon} alt="" className='img-responsive' width={200} />
+          <h1 className='display-6'>Brennevin</h1>
+          <Link to="/Brennevin" className='btn btn-primary'>Se mer</Link>
+        </div>
+        <div className='col-lg border p-3'>
+        <img src={Icon} alt="" className='img-responsive' width={200} />
+          <h1 className='display-6'>Sterkvin</h1>
+          <Link to="/Sterkvin" className='btn btn-primary'>Se mer</Link>
+        </div>
+        <div className='col-lg border p-3'>
+          <img src={Icon} alt="" className='img-responsive' width={200} />
+          <h1 className='display-6'>Vin</h1>
+          <Link to="/Vin" className='btn btn-primary'>Se mer</Link>
+        </div>
+        <div className='col-lg border p-3'>
+          <img src={Icon} alt="" className='img-responsive' width={200} />
+          <h1 className='display-6'>Øl</h1>
+          <Link to="/Øl" className='btn btn-primary'>Se mer</Link>
+        </div>
+        <div className='col-lg border p-3'>
+          <img src={Icon} alt="" className='img-responsive' width={200} />
+          <h1 className='display-6'>Cider</h1>
+          <Link to="/Cider" className='btn btn-primary'>Se mer</Link>
+        </div>
+      </div>
     </div>
   )
 }
