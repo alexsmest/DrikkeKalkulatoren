0b0e521abc636afac416f40c9f7053980148f8dc
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 13:25:43 +0200

    Update Header.js

--- drikke-kalkulator/src/components/Header.js
+++ drikke-kalkulator/src/components/Header.js
@@ -1,44 +1,36 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import Icon from "../images/flaske.png"
 import ØlIcon from "../images/carlsberg.png"
 import VinIcon from "../images/vinm.png"
-import SterkVin from "../images/cockburn.png"
+import SterkvinIcon from "../images/cockburn.png"
 import BrennevinIcon from "../images/brennevin.png"
 import CiderIcon from "../images/cider.png"
 
 function Header() {
   return (
     <div className='container'>
-      <h1 className='display-4 mt-5'>DrikkeKalkulatoren</h1>
-      <hr />
-      <p>Drikk deg dritings</p>
+      <h1 className='display-4 text-center mt-5 container bg-dark text-white pt-3 pb-4 shadow'>Utvalg</h1>
       <div className='row text-center mt-5'>
-        <div className='col-lg border p-3'>
-          <img src={BrennevinIcon} alt="" className='img-responsive' height={200} />
-          <h1 className='display-6'>Brennevin</h1>
-          <Link to="/Brennevin" className='btn btn-primary'>Se utvalg</Link>
-        </div>
-        <div className='col-lg border p-3'>
-        <img src={SterkVin} alt="" className='img-responsive' height={200} />
-          <h1 className='display-6'>Sterkvin</h1>
-          <Link to="/Sterkvin" className='btn btn-primary'>Se utvalg</Link>
-        </div>
-        <div className='col-lg border p-3'>
-          <img src={VinIcon} alt="" className='img-responsive' height={200} /> 
-          <h1 className='display-6'>Vin</h1>
-          <Link to="/Vin" className='btn btn-primary'>Se utvalg</Link>
-        </div>
-        <div className='col-lg border p-3'>
-          <img src={ØlIcon} alt="" className='img-responsive' height={200} />
-          <h1 className='display-6'>Øl</h1>
-          <Link to="/Øl" className='btn btn-primary'>Se utvalg</Link>
-        </div>
-        <div className='col-lg border p-3'>
+        <Link to="/Cider" className='col-lg border p-3 text-decoration-none'>
           <img src={CiderIcon} alt="" className='img-responsive' height={200} />
-          <h1 className='display-6'>Cider</h1>
-          <Link to="/Cider" className='btn btn-primary'>Se utvalg</Link>
-        </div>
+          <h1 className='display-6 text-dark'>Cider</h1>
+        </Link>
+        <Link to="/Øl" className='col-lg border p-3 text-decoration-none'>
+          <img src={ØlIcon} alt="" className='img-responsive' height={200} />
+          <h1 className='display-6 text-dark'>Øl</h1>
+        </Link>
+        <Link to="/Vin" className='col-lg border p-3 text-decoration-none'>
+          <img src={VinIcon} alt="" className='img-responsive' height={200} />
+          <h1 className='display-6 text-dark'>Vin</h1>
+        </Link>
+        <Link to="/Brennevin" className='col-lg border p-3 text-decoration-none'>
+          <img src={BrennevinIcon} alt="" className='img-responsive' height={200} />
+          <h1 className='display-6 text-dark'>Brennevin</h1>
+        </Link>
+        <Link to="/Sterkvin" className='col-lg border p-3 text-decoration-none'>
+          <img src={SterkvinIcon} alt="" className='img-responsive' height={200} />
+          <h1 className='display-6 text-dark'>Sterkvin</h1>
+        </Link>
       </div>
     </div>
   )
