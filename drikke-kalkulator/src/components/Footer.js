0c3abab82ce1e75708d08c8d5c05b19ae9d666e8
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-13 10:00:48 +0200

    Update Footer.js

--- drikke-kalkulator/src/components/Footer.js
+++ drikke-kalkulator/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-dark text-center mt-5 shadow fixed-bottom'>
+    <div className='bg-dark text-center mt-5 shadow'>
         <div className='container text-white pt-4 pb-2'>
             <p>&copy; DrikkeKalkulatoren | Laget av Ole, Alexander og Frederik</p>
         </div>
