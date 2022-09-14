5f029ba57d5930cda9b11124f7de8ef7721fc7fc
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-14 10:17:03 +0200

    Update Cider.js

--- src/components/Produkter/Cider.js
+++ src/components/Produkter/Cider.js
@@ -15,13 +15,17 @@ function Cider() {
           <p>Sortert basert på merke</p>
         </div>
         <div className='text-center'>
-          <button className='btn btn-warning mb-2' data-bs-toggle="collapse" data-bs-target="#grevens">Grevens</button>
-          <div className='row collapse' id='grevens'>
-            {
-              Cidere.map((x, i) => {
-                return <ProductItem name={Cidere[i].name} type={Cidere[i].type} icon={Cidere[i].icon} uid={Cidere[i].uid} price={Cidere[i].price} percent={Cidere[i].percent} size={Cidere[i].size} />
-              })
-            }
+          <div className='bg-dark py-3' role="button" data-bs-toggle="collapse" data-bs-target="#grevens">
+            <h1 className='display-5 text-white'>Grevens</h1>
+          </div>
+          <div className='container'>
+            <div className='row collapse' id='grevens'>
+              {
+                Cidere.map((x, i) => {
+                  return <ProductItem name={Cidere[i].name} type={Cidere[i].type} icon={Cidere[i].icon} uid={Cidere[i].uid} price={Cidere[i].price} percent={Cidere[i].percent} size={Cidere[i].size} />
+                })
+              }
+            </div>
           </div>
         </div>
       </div>
