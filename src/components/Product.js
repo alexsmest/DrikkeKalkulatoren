84df16909e047a8b0561c9ad86c387397a8419ba
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-14 11:45:51 +0200

    fiksa sortering

---
+++ src/components/Product.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import ProductItem from './Produkter/ProductItem'
+
+function Product(props) {
+  return (
+    <div className='text-center'>
+        <h1 className='container display-6 bg-dark text-white py-3 mt-2' data-bs-toggle="collapse" data-bs-target={"#" + props.id}>{props.id.toUpperCase()}</h1>
+        <div className='container collapse row' id={props.id}>
+        {
+            props.product.map((x, i) => {
+            return (
+                <ProductItem name={props.product[i].name} type={props.product[i].type} icon={props.product[i].icon} price={props.product[i].price} percent={props.product[i].percent} size={props.product[i].size} />
+            )
+            })
+        }
+        </div>
+    </div>
+  )
+}
+
+export default Product
--- src/components/Produkter/Cider.js
+++ src/components/Produkter/Cider.js
@@ -1,27 +1,10 @@
 import React from 'react'
 
 import ProdukterNav from '../ProdukterNav'
-import ProductItem from './ProductItem'
+import Product from '../Product'
 
 import Grevens from "../../data/Cider/Grevens.json"
 import Crush from "../../data/Cider/Crush.json"
-
-function Product(product, id) {
-  return(
-  <div className='text-center'>
-    <h1 className='container display-6 bg-dark text-white py-3 mt-2' data-bs-toggle="collapse" data-bs-target={"#" + id}>{id.toUpperCase()}</h1>
-    <div className='container collapse row' id={id}>
-      {
-        product.map((x, i) => {
-          return (
-            <ProductItem name={product[i].name} type={product[i].type} icon={product[i].icon} price={product[i].price} percent={product[i].percent} size={product[i].size} />
-          )
-        })
-      }
-    </div>
-  </div>
-  )
-}
 
 function Cider() {
   return (
@@ -32,8 +15,8 @@ function Cider() {
           <h1 className='display-4'>Legg til cidere</h1>
           <p>Sortert basert på merke</p>
         </div>
-        {Product(Grevens, "grevens")}
-        {Product(Crush, "crush")}
+        <Product product={Grevens} id="grevens" />
+        <Product product={Crush} id="crush" />
       </div>
     </>
   )
