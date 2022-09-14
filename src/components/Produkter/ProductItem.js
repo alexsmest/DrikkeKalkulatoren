742bd12d86278b434e36585e2a99feb2ea0bcc07
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-14 09:58:54 +0200

    Update ProductItem.js

--- src/components/Produkter/ProductItem.js
+++ src/components/Produkter/ProductItem.js
@@ -6,14 +6,12 @@ function ProductItem(props) {
         <img src={props.icon} alt="" height={200} />
         <p className='fw-bold'>{props.name}</p>
         <p>{props.type}</p>
-        <button className='btn btn-warning mb-2'>Legg til</button>
-        <div className='form-group'>
-          <button className='btn btn-warning' data-bs-toggle="collapse" data-bs-target={"#" + props.id}>Se detaljer</button>
-          <div className='collapse' id={props.id}>
-              <p className='mb-0 mt-2'>Pris: {props.price + "kr"}</p>
-              <p className='mb-0'>Alkoholprosent: {props.percent + "%"}</p>
-              <p className='mb-0'>Innhold: {props.size + " liter"}</p>
-          </div>
+        <button className='btn btn-warning me-2'>Legg til</button>
+        <button className='btn btn-warning' data-bs-toggle="collapse" data-bs-target={"#" + props.id}>Se detaljer</button>
+        <div className='collapse' id={props.id}>
+            <p className='mb-0 mt-2'>Pris: {props.price + "kr"}</p>
+            <p className='mb-0'>Alkoholprosent: {props.percent + "%"}</p>
+            <p className='mb-0'>Innhold: {props.size + " liter"}</p>
         </div>
     </div>
   )
