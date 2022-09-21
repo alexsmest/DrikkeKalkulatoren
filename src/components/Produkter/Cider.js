ca60db26d2192244899c9d818d0490bb73c438f1
FrederikWest <frederikwest@outlook.com>
2022-09-21 11:22:25 +0200

    d

--- src/components/Produkter/Cider.js
+++ src/components/Produkter/Cider.js
@@ -8,7 +8,6 @@ import Grevens from "../../data/Cider/Grevens.json"
 import Crush from "../../data/Cider/Crush.json"
 import Barecider from "../../data/Cider/Barecider.json"
 import Sommersby from "../../data/Cider/Sommersby.json"
-import SmirnoffIce from "../../data/Cider/SmirnoffIce.json"
 import Breezer from "../../data/Cider/Breezer.json"
 import Annet from "../../data/Cider/AnnetCider.json"
 
@@ -23,7 +22,6 @@ function Cider() {
         <Product product={Barecider} id="Barecider" />
         <Product product={Sommersby} id="Sommersby" />
         <Product product={Breezer} id="Breezer" />
-        <Product product={SmirnoffIce} id="Smirnoff" />
         <Product product={Annet} id="Annet" />
       </div>
     </>
