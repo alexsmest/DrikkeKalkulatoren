81197d8242d69fe084383e6202fad5cdd7c5af19
Ole Morten Halvorsen <ole2005morten@outlook.com>
2022-09-21 15:24:33 +0200

    Update ProductItem.js

--- src/components/Produkter/ProductItem.js
+++ src/components/Produkter/ProductItem.js
@@ -12,7 +12,9 @@ function ProductItem(props) {
         <img src={props.icon} alt="" height={200} />
         <p className='fw-bold'>{props.name}</p>
         <p>{props.type}</p>
-        <button className='btn btn-warning' onClick={() => Handlekurv.push([props.name + " " + props.type, props.price, props.percent, props.size, KalkulerAlkoholGram(props.size, props.percent)])}>Legg til</button>
+        <button className='btn btn-warning position-relative' onClick={() => Handlekurv.push([props.name + " " + props.type, props.price, props.percent, props.size, KalkulerAlkoholGram(props.size, props.percent)])}>Legg til
+          <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark' onClick={antall += 1}>{antall}</span>
+        </button>
         <p className='mb-0 mt-2'>Pris: {props.price + "kr"}</p>
         <p className='mb-0'>Alkoholprosent: {props.percent + "%"}</p>
         <p className='mb-0'>Innhold: {props.size + " liter"}</p>
